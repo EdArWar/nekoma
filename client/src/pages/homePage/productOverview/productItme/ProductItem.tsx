c0463e87a5d91b54c8ba3770c409caccf5f2eb56
EdWard <edgar.vard92@gmail.com>
2021-04-09 15:55:15 +0400

    add icons

--- client/src/pages/homePage/productOverview/productItme/ProductItem.tsx
+++ client/src/pages/homePage/productOverview/productItme/ProductItem.tsx
@@ -3,7 +3,16 @@ import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import "./ProductItem.scss";
 import { IProductDataConfig } from "./../../../../types/ProductType";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCartPlus,
+  faHeart,
+  faSignInAlt,
+} from "@fortawesome/free-solid-svg-icons";
 const ProductItem: React.FC<IProductDataConfig> = ({ configs }) => {
+  console.log("ProductItem");
+  console.log(configs);
+
   return (
     <div
       className={`col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item  h_card_item_show ${configs.productTag.toLowerCase()}`}
@@ -26,21 +35,35 @@ const ProductItem: React.FC<IProductDataConfig> = ({ configs }) => {
             Quick View
           </NavLink>
         </NavLink>
-        <div className="block2-txt flex-w flex-t p-t-14">
-          <div className="block2-txt-child1 flex-col-l ">
+        <div
+          className="block2-txt flex-w flex-t p-t-14 row"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-start",
+          }}
+        >
+          <div className="  col-lg-8 col-md-8 col-sm-8 col-xs-12 ">
             <NavLink
               to={`/singleProduct?id=${configs._id}`}
               className="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6"
             >
               {configs.productTitle}
             </NavLink>
-            <span className="stext-105 cl3">${configs.productPrice}</span>
+            <p className="stext-105 cl3">${configs.productPrice}</p>
           </div>
-          <div className="block2-txt-child2 flex-r p-t-3">
-            <NavLink
-              to={`/singleProduct?id=${configs._id}`}
-              className="btn-addwish-b2 dis-block pos-relative js-addwish-b2"
-            ></NavLink>
+          <div
+            className="col-lg-4 col-md-4 col-sm-4 col-xs-12"
+            style={{ display: "flex", justifyContent: "space-around" }}
+          >
+            <FontAwesomeIcon
+              icon={faHeart}
+              style={{ color: "#717FDF", fontSize: "22px", cursor: "pointer" }}
+            />
+            <FontAwesomeIcon
+              icon={faCartPlus}
+              style={{ color: "#717FDF", fontSize: "22px", cursor: "pointer" }}
+            />
           </div>
         </div>
       </div>
