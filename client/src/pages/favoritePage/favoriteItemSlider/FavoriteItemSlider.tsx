625cc19886b18094e949315ecc751c05f1cdd139
EdWard <edgar.vard92@gmail.com>
2021-04-11 20:37:22 +0400

    favorit card fix

--- client/src/pages/favoritePage/favoriteItemSlider/FavoriteItemSlider.tsx
+++ client/src/pages/favoritePage/favoriteItemSlider/FavoriteItemSlider.tsx
@@ -1,13 +1,42 @@
+import {
+  faCartArrowDown,
+  faCartPlus,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
 import React, { RefObject, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import Slider from "react-slick";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   IProductDataConfig,
   IProductImage,
 } from "./../../../types/ProductType";
+import { api_addToCart, api_addToFavorite } from "../../../api/API";
+import { api_removeFavorite, api_removeCart } from "./../../../api/API";
+import { NEKOMA } from "./../../../style/Nekoma";
+import "./FavoriteItemSlider.scss";
 
 const FavoriteItemSlider: React.FC<IProductDataConfig> = ({ configs }) => {
   const [state, setState] = useState(false);
+  const userCart = useSelector((state: any) => state.user.userCart);
+  const userFavorite = useSelector((state: any) => state.user.favorites);
+  const [added, setAdded] = useState(false);
+  const [addedFavorite, setAddedFavorite] = useState(false);
+  const isUser = useSelector((state: any) => state.global.isUser);
+  const dispatch = useDispatch();
+  const token = useSelector((state: any) => state.user.token);
+
+  useEffect(() => {
+    const isCard = userCart?.find((item: any, i: number) => {
+      return item._id === configs._id;
+    });
+    const isFavorite = userFavorite?.find((item: any, i: number) => {
+      return item._id === configs._id;
+    });
+    !isUser ? setAdded(false) : setAdded(isCard);
+    !isUser ? setAddedFavorite(false) : setAddedFavorite(isFavorite);
+  }, [userCart, isUser, userFavorite]);
 
   useEffect(() => {}, [state]);
 
@@ -20,6 +49,32 @@ const FavoriteItemSlider: React.FC<IProductDataConfig> = ({ configs }) => {
     slidesToScroll: 1,
     autoplay: true,
   };
+
+  function onAddCartClicked() {
+    console.log("onAddCartClicked");
+    if (!isUser) {
+      console.log("Need To Registration");
+    } else {
+      dispatch(api_addToCart(token, configs._id));
+      !isUser ? setAdded(false) : setAdded(!added);
+    }
+  }
+  function onAddFavoriteClicked() {
+    console.log("onAddCartClicked");
+    if (!isUser) {
+      console.log("Need To Registration");
+    } else {
+      dispatch(api_addToFavorite(token, configs._id));
+      !isUser ? setAddedFavorite(false) : setAddedFavorite(!added);
+    }
+  }
+
+  function onRemoveCart(cartId: string) {
+    dispatch(api_removeCart(token, cartId));
+  }
+  function onRemoveFavorite(cartId: string) {
+    dispatch(api_removeFavorite(token, cartId));
+  }
 
   return (
     <>
@@ -69,11 +124,64 @@ const FavoriteItemSlider: React.FC<IProductDataConfig> = ({ configs }) => {
         <a className="mc-btn-action" onClick={() => setState(!state)}>
           <i className="fa fa-bars"></i>
         </a>
-        <div className="mc-footer">
-          <a className="fa fa-fw fa-facebook"></a>
-          <a className="fa fa-fw fa-twitter"></a>
-          <a className="fa fa-fw fa-linkedin"></a>
-          <a className="fa fa-fw fa-google-plus"></a>
+        <div
+          className="mc-footer"
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <div className="btn-container">
+            {addedFavorite ? (
+              <FontAwesomeIcon
+                icon={faHeart}
+                className="proxz-btn"
+                style={{
+                  color: NEKOMA.RED,
+                  fontSize: "22px",
+                  cursor: "pointer",
+                }}
+                onClick={() => onRemoveFavorite(configs._id)}
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faHeart}
+                className="proxz-btn"
+                style={{
+                  color: "#717FDF",
+                  fontSize: "22px",
+                  cursor: "pointer",
+                }}
+                onClick={() => onAddFavoriteClicked()}
+              />
+            )}
+          </div>
+          <div className="btn-container">
+            {added ? (
+              <FontAwesomeIcon
+                icon={faCartArrowDown}
+                className="proxz-btn"
+                style={{
+                  color: "#00DA00",
+                  fontSize: "22px",
+                  cursor: "pointer",
+                }}
+                onClick={() => onRemoveCart(configs._id)}
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faCartPlus}
+                className="proxz-btn"
+                style={{
+                  color: "#717FDF",
+                  fontSize: "22px",
+                  cursor: "pointer",
+                }}
+                onClick={() => onAddCartClicked()}
+              />
+            )}
+          </div>
         </div>
       </article>
     </>
